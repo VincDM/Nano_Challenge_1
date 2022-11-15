6d51788c4da71dbd116d3e387d8f40b10db66c7e
VincDM <113534568+VincDM@users.noreply.github.com>
2022-11-15 15:32:58 +0100

    First commit

---
+++ NC1/NC1App.swift
@@ -0,0 +1,17 @@
+//
+//  NC1App.swift
+//  NC1
+//
+//  Created by Vincenzo Di Meo on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
